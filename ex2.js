e69b0a044631e073e745fc6541d7d4eb94af9985
tiju46 <53434126+tiju46@users.noreply.github.com>
2026-05-01 13:01:30 +0100

    Update ex2.js

--- ex2.js
+++ ex2.js
@@ -9,3 +9,12 @@ function showResult() {
 
   alert("Sum is: " + sum);
 }
+
+function addnumbers() {
+  let num1 = Number(document.getElementById("num1").value);
+  let num2 = Number(document.getElementById("num2").value);
+
+  let sum = num1 + num2;
+
+  alert("Total is: " + sum);
+}
